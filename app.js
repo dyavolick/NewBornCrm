43c87db24101abe539730425af2f6074203fe921
dyavolick <dyavolick@gmail.com>
2020-05-11 03:58:11 +0300

    add new route

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const analyticsRouts = require('./routes/analytics')
 const authRouts = require('./routes/auth')
+const categoryRouts = require('./routes/category')
+const orderRouts = require('./routes/order')
+const positionRouts = require('./routes/position')
 const app = express()
 
+app.use('/api/analytics', analyticsRouts)
 app.use('/api/auth', authRouts)
+app.use('/api/category', categoryRouts)
+app.use('/api/order', orderRouts)
+app.use('/api/position', positionRouts)
 
 module.exports = app
