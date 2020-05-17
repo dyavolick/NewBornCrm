00e9732ec0e9109d3a28805ec5420af9e115937a
dyavolick <dyavolick@gmail.com>
2020-05-17 04:51:46 +0300

    add auntificate

---
+++ midldleware/passport.js
@@ -0,0 +1,26 @@
+const JwtStrategy = require('passport-jwt').Strategy,
+      ExtractJwt = require('passport-jwt').ExtractJwt;
+const mongoose = require('mongoose')
+const keys = require('../config/keys')
+const User = mongoose.model('users')
+
+const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: keys.jwt
+}
+
+module.exports = passport=> {
+    passport.use(
+        new JwtStrategy(opts, async (payload, done)=> {
+            try {
+            const user = await User.findById({id: payload.userId}).select('email id')
+            if(user){
+                done(null, user)
+            } else {
+                done(null, false)
+            }} catch (e) {
+                console.log(e)
+            }
+        })
+    )
+}
