04263d993bb11cda2cef8d6fc5f1f7e22f78a526
dyavolick <dyavolick@gmail.com>
2020-05-19 04:07:18 +0300

    update position

--- routes/position.js
+++ routes/position.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const controller = require('../controllers/position')
+const passport = require('passport')
 const router = express.Router()
 
-router.get('/:categoryId',controller.getByCategoryId)
+router.get('/:categoryId',
+    passport.authenticate('jwt', { session: false}),
+    controller.getByCategoryId)
 router.post('/',controller.create)
 router.patch('/:id',controller.update)
 router.post('/:id',controller.remove)
